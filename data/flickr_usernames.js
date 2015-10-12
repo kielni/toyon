a7f5df35843c187db3e5b0b3f96e6e25f3b89418
Kimberly Nicholls <kimberly@gridium.com>
2015-10-11 19:44:50 -0700

    get owner id and usernames for flickr photos in favorites

---
+++ data/flickr_usernames.js
@@ -0,0 +1,60 @@
+/*
+    get Flickr usernames by id for use in CC attributions
+*/
+var async = require("async");
+var Flickr = require("flickrapi");
+var flickrOptions = {
+        api_key: process.env.FLICKR_API_KEY,
+        secret: process.env.FLICKR_API_SECRET,
+        permissions: "read"
+};
+var owners = {};
+var photos = {};
+
+var getUsername = function(flickr, ownerId, callback) {
+    if (owners[ownerId]) {
+        callback();
+        return;
+    }
+    flickr.people.getInfo({user_id: ownerId}, function(err, result) {
+        owners[ownerId] = result.person.username._content;
+        console.log("\nowner "+ownerId+" = "+owners[ownerId]);
+        callback(null, owners[ownerId]);
+    });
+};
+
+var getFavoritesPage = function(flickr, page, callback) {
+    flickr.favorites.getPublicList({
+        user_id: process.env.FLICKR_USER_ID,
+        page: page
+    }, function(err, result) {
+        async.forEachSeries(result.photos.photo, function(photo, forEachCallback) {
+            if (!photos[photo.id]) {
+                photos[photo.id] = {
+                    owner: photo.owner
+                };
+            }
+            getUsername(flickr, photo.owner, function(err, result) {
+                photos[photo.id].username = result;
+                forEachCallback();
+            });
+        }, function(err) {
+            // got all usernames for this page
+            callback(null, result.photos.page, result.photos.pages);
+        });
+    });
+};
+
+Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+    getFavoritesPage(flickr, 1, function(err, page, totalPages) {
+        console.log("got favorites page 1/"+totalPages);
+        async.timesSeries(totalPages-1, function(n, next) {
+            console.log("\n\n*** get page "+(n+1));
+            getFavoritesPage(flickr, n+1, next);
+        }, function(err) {
+            console.log("json=");
+            console.log(JSON.stringify({"flickrPhotos": photos}));
+            system.exit(0);
+        });
+    });
+});
