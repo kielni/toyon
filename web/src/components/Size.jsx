d048d99bf91b5bb439abdbff500104b167ad19ca
Kimberly Nicholls <kimberly@gridium.com>
2015-11-01 11:20:30 -0800

    remove extra '

--- web/src/components/Size.jsx
+++ web/src/components/Size.jsx
@@ -6,7 +6,7 @@ export default React.createClass({
             <div className="size">
                 <span className="height">{this.props.height.from}-{this.props.height.to}' tall</span>
                 <span className="by">x</span>
-                <span className="width">{this.props.spread.from}-{this.props.spread.to}'' wide</span>
+                <span className="width">{this.props.spread.from}-{this.props.spread.to}' wide</span>
             </div>
         );
     }
