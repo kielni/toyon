f36298dc4426600bab813bdb198d2df781c63e70
Kimberly Nicholls <kimberly@gridium.com>
2015-11-01 11:18:49 -0800

    get data from S3 instead of Firebase
    data file is static so it's much faster to get all of it gzipped with cache headers than one record at a time from Firebase

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { Link }  from 'react-router';
 import { Navbar, NavBrand, Nav, NavItem } from 'react-bootstrap';
 import PlantList from './PlantList.jsx';
+import Request from 'superagent';
 
 export default React.createClass({
-    mixins: [ReactFireMixin],
+    getInitialState: function() {
+        return {
+            plants: []
+        };
+    },
 
-    componentWillMount: function() {
-        var ref = new Firebase('https://toyon.firebaseio.com/plants/').limit(20);
-        this.bindAsArray(ref, 'plants');
-      },
+    componentDidMount() {
+        var self = this;
+        Request.get('https://s3-us-west-1.amazonaws.com/toyon/toyon_pruned.json.gz')
+            .end(function(err, res){
+                self.setState({plants: res.body.plants});
+        });
+    },
 
     render() {
         var filtered = this.state.plants.slice(0, 10);
