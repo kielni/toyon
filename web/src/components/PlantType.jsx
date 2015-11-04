3dd5d3b040a3eef2e2d41784dc353c5f8fa2ad2c
Kimberly Nicholls <kimberly@gridium.com>
2015-11-04 12:31:49 -0800

    plant type text and size bucket icon

---
+++ web/src/components/PlantType.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default React.createClass({
+    labels: {
+        'B': 'bulb',
+        'Gr': 'grass',
+        'Gc': 'groundcover',
+        'P': 'perennial',
+        'S': 'shrub',
+        'T': 'tree',
+        'V': 'vine'
+    },
+
+    render() {
+        var plantType = this.props.type.split(/\s+/).map((code) => {
+            var cls = 'type '+code;
+            return (
+                <li className={cls} key={code}>{this.labels[code]}</li>
+            );
+        });
+        return (
+            <ul className="plant-type">
+                {plantType}
+            </ul>
+        );
+    }
+});
---
+++ web/src/components/SizeBucket.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+
+export default React.createClass({
+    render() {
+        var cls = 'size '+this.props.bucket;
+        var tooltip = (<Tooltip>{this.props.bucket}</Tooltip>);
+        return (
+            <div className="size-req">
+                <OverlayTrigger key={this.props.bucket} placement="bottom" overlay={tooltip}>
+                    <span className={cls}></span>
+                </OverlayTrigger>
+            </div>
+        );
+    }
+});
+
