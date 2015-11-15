6de05b45da6c0cd9cce220bea7b7e4c7e2264125
Kimberly Nicholls <kimberly@gridium.com>
2015-11-14 19:04:45 -0800

    don't remove search results

--- scripts/prune.js
+++ scripts/prune.js
@@ -14,14 +14,14 @@ var plants = plantdb.plants;
 
 // height buckets: 1, 3, 10
 var heightBuckets = [1, 3, 10, 999];
-var heightLabels = {1: 'low', 3: 'small', 10: 'medium', 999: 'large'};
+var heightLabels = {1: "low", 3: "small", 10: "medium", 999: "large"};
 
 var pruned = [];
 plants.forEach(function(plant) {
     if (!plant.fullInfo) {
         return;
     }
-    plant.water = { 'range': plant.water.range };
+    plant.water = { "range": plant.water.range };
     ["missouriBotanical", "sanMarcos"].forEach(function(src) {
         ["height", "spread", "sun"].forEach(function(field) {
             if (plant[field]) {
@@ -40,9 +40,6 @@ plants.forEach(function(plant) {
         });
     });
     delete plant.fullInfo;
-    if (plant.search) {
-        delete plant.search;
-    }
     pruned.push(plant);
 });
 console.log(JSON.stringify({"plants": pruned}, null, 2));
